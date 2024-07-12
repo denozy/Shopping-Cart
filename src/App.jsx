6bc53a54755412473d57a69e6d602887c6127476
Nick <ndenbleyker@gmail.com>
2024-07-12 11:43:25 -0400

    good project template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import "./index.css";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<NotFoundPage />}>
+        <Route index element={<HomePage />} /> //index is route element that
+        displays when no other child is selected. default.
+        <Route path="*" element={<NotFoundPage />} /> //i think this makes it so
+        any page thats not the listed as child goes here.
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
 
 export default App;
