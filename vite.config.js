ee4c2cb327315b63d68a116136ec40fd88e56f8c
Nick <ndenbleyker@gmail.com>
2024-08-05 17:02:27 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Shopping-Cart/",
+  base: "/Shopping-Cart/", // Update to match your repository name
 });
